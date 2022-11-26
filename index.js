4a883f7752574e1776b1b68f52fb6dcd21df5461
Faith Moraa <moraaf770@gmail.com>
2022-11-26 23:51:53 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(actiivity ='roller-skate'){
+    return (`This Saturday, I want to ${actiivity}!`)
+}
+saturdayFun('roller-skate');
+
+function mondayWork(activity = 'go to office'){
+    return (`This Monday, I will ${activity}.`);
+
+}
+mondayWork('work from home.');
+
+function wrapAdjective(wrapper = "*"){
+    function message(string = "special"){
+        return `You are ${wrapper}${string}${wrapper}!`;
+    };
+    return message;
+};
+wrapAdjective("||");
+result("a hard worker");
+
